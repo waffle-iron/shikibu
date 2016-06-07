94f9faf78b86e251f583594f2f5725639c9dc87d
nukosuke <39.nuko@gmail.com>
2016-06-07 21:46:05 +0900

    import commands

--- src/bin/shikibu.ts
+++ src/bin/shikibu.ts
@@ -1,10 +1,13 @@
-import * as program from "commander";
+#!/usr/bin/env node
+import * as program from 'commander';
+import * as commands from '../commands';
+
 
 program
-  .command('new <name>', 'create new monogatari')
+  .command('create <name>')
+  .alias('new')
+  .description('create new monogatari')
   .option('-t, --template <template>', 'template directory for new monogatari')
-  .action((name, template) => {
-
-  });
+  .action(commands.create);
 
 program.parse(process.argv);
